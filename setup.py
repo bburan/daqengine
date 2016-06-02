a4d09d0c5fbe0623c5ceae104dc681503ae62283
Brad Buran <buran@ohsu.edu>
2016-06-02 15:20:37 -0700

    ENH: Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='DAQengine',
+    version='0.0.1',
+    author='Brad Buran',
+    author_email='bburan@alum.mit.edu',
+    packages=['daqengine'],
+    description='Module for interfacing with DAQ cards',
+)
